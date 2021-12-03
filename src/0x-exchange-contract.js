145f03a4026030681a1718fc39f0495056a930dd
dcpp <dmitriy.dolinin.v@gmail.com>
2021-12-03 13:45:14 +0300

    Token->ETH swap tests

--- src/0x-exchange-contract.js
+++ src/0x-exchange-contract.js
@@ -20,7 +20,7 @@ let tokenAddresses = {};
 
 async function callZRXSwapAPI(qs, network) {
     const quoteUrl = `${NETWORKS_0X_API_URL[network]}swap/v1/quote?${qs}`;
-    // console.info(`Fetching quote ${quoteUrl.bold}...`);
+    console.info(`Fetching quote ${quoteUrl.bold}...`);
     const response = await fetch(quoteUrl);
     const quote = await response.json();
     if (!response.ok) {
@@ -204,7 +204,7 @@ async function testHackSwap(web3, network, sellAmount, sellTokenName, buyTokenNa
 }
 
 
-async function doSwapNative(web3, network, sellAmount, nativeToken, buyTokenName) {
+async function doSwapSellNative(web3, network, sellAmount, nativeToken, buyTokenName) {
     const deployedAddress = TEST_CONFIG[network].deployedAddress;
     const contract = new web3.eth.Contract(ABIEX, deployedAddress);
     const [owner] = await web3.eth.getAccounts();
@@ -280,6 +280,94 @@ async function doSwapNative(web3, network, sellAmount, nativeToken, buyTokenName
     }
 }
 
+async function doSwapBuyNative(web3, network, sellAmount, sellTokenName, nativeToken) {
+    const deployedAddress = TEST_CONFIG[network].deployedAddress;
+    const contract = new web3.eth.Contract(ABIEX, deployedAddress);
+    const [owner] = await web3.eth.getAccounts();
+
+    // fake api call to get sell and buy tokens addresses
+    let quote = await callZRXSwapAPI(createQueryString({
+        sellToken: sellTokenName,
+        buyToken: `W${nativeToken}`,
+        sellAmount: etherToWei(1)
+    }), network);
+
+    tokenAddresses[sellTokenName] = quote.sellTokenAddress;
+    tokenAddresses[`W${nativeToken}`] = quote.buyTokenAddress;
+
+    const sellToken = new web3.eth.Contract(ABIERC20, quote.sellTokenAddress);
+    const buyToken = new web3.eth.Contract(ABIERC20, quote.buyTokenAddress);
+
+    const sellTokenDecimals = await sellToken.methods.decimals().call();
+    const buyTokenDecimals = await buyToken.methods.decimals().call();
+
+    let sellAmountBase = amountToBase(sellAmount, sellTokenDecimals);
+
+    // Get a quote from 0x-API to sell
+    quote = await callZRXSwapAPI(createQueryString({
+        sellToken: sellTokenName,
+        buyToken: `W${nativeToken}`,
+        sellAmount: sellAmountBase,
+    }), network);
+
+    var str = JSON.stringify(quote, null, 2);
+    // console.log(str)
+    console.info(`Received a quote with best price 1 ${sellTokenName} = ${quote.price} ${nativeToken}, and guaranteed price 1 ${sellTokenName} = ${quote.guaranteedPrice} ${nativeToken}`);
+    console.info(`Fetching swap quote from 0x-API to buy ${baseToAmount(quote.buyAmount, buyTokenDecimals)} ${nativeToken} for ${baseToAmount(quote.sellAmount, sellTokenDecimals)} ${sellTokenName}`);
+
+    console.log(`${sellTokenName} addr: ${quote.sellTokenAddress}, W${nativeToken} addr: ${quote.buyTokenAddress}`);
+
+    const swapTarget = await contract.methods.getSwapTarget().call();
+
+    if (swapTarget.toString().toLowerCase() != quote.to.toString().toLowerCase()) {
+        console.log(`Swap targets is not equal. In contract: ${swapTarget}, in API: ${quote.to}`);
+        process.exit(1);
+    }
+
+    // check sellTokenBalance
+    const result = await sellToken.methods.balanceOf(owner).call();
+    if (result < sellAmountBase) {
+        console.log(`Insufficient sell token funds: need ${sellAmountBase}, have ${result}`);
+        process.exit(1);
+    }
+
+    let receipt;
+    try {
+        // aprove exchange contract to tranfer sell token from owner to self
+        receipt = await waitForTxSuccess(sellToken.methods.approve(
+            deployedAddress,
+            sellAmountBase
+        ).send({
+            from: owner,
+            gasPrice: quote.gasPrice
+        })).catch(err => {
+            throw new Error(err)
+        });
+
+        // Have the contract fill the quote
+        console.info(`Filling the quote through the contract at ${deployedAddress.bold}...`);
+        receipt = await waitForTxSuccess(contract.methods.fillQuoteBuyETH(
+                sellAmountBase,
+                quote.sellTokenAddress,
+                quote.allowanceTarget,
+                quote.data
+        ).send({
+            from: owner,
+            value: quote.value + sellAmountBase,
+            gasPrice: quote.gasPrice,
+            gasLimit: '1000000'
+        })).catch(err => {
+            console.log(err);
+            throw err;
+        });
+        const boughtAmount = baseToAmount(receipt.events.BoughtTokens.returnValues.boughtAmount, buyTokenDecimals);
+        console.info(`${'✔'.bold.green} Successfully sold ${baseToAmount(sellAmountBase, sellTokenDecimals).toString().bold} ${sellTokenName} for ${boughtAmount.bold.green} ${nativeToken}!`);
+    }
+    catch (err) {
+        process.exit(1);
+    }
+}
+
 async function doWithdrawFee(web3, tokenName, recipientAddress) {
     const deployedAddress = TEST_CONFIG[network].deployedAddress;
     const contract = new web3.eth.Contract(ABIEX, deployedAddress);
@@ -342,13 +430,32 @@ async function test2(network) {
     const token2 = TEST_CONFIG[network].token2;
     const addressForFee = TEST_CONFIG[network].addressForFee;
 
-    await doSwapNative(web3, network, sellAmount, nativeToken, token1);
-    await doSwapNative(web3, network, sellAmount, nativeToken, token2);
+    await doSwapSellNative(web3, network, sellAmount, nativeToken, token1);
+    await doSwapSellNative(web3, network, sellAmount, nativeToken, token2);
 
     await doWithdrawFee(web3, token2, addressForFee);
     await doWithdrawFee(web3, token1, addressForFee);
 
     console.info('Test exchange ETH to ERC20 token success');
+}
+
+async function test3(network) {
+    console.info('Start test to exchange ERC20 token to ETH');
+    const web3 = createWeb3(network);
+
+    const sellAmount = TEST_CONFIG[network].sellAmount;
+    const nativeToken = TEST_CONFIG[network].nativeToken;
+    const wToken = TEST_CONFIG[network].wrappedToken;
+    const token1 = TEST_CONFIG[network].token1;
+    const token2 = TEST_CONFIG[network].token2;
+    const addressForFee = TEST_CONFIG[network].addressForFee;
+
+    await doSwapBuyNative(web3, network, sellAmount, token1, nativeToken);
+    await doSwapBuyNative(web3, network, sellAmount, token2, nativeToken);
+
+    await doWithdrawFee(web3, wToken, addressForFee);
+
+    console.info('Test exchange ERC20 token to ETH success');
 }
 
 let network = process.argv.slice(2)[0];
@@ -362,6 +469,7 @@ if (Object.keys(NETWORKS_0X_API_URL).indexOf(network) === -1) {
 async function runTest(network) {
     await test1(network);
     await test2(network);
+    await test3(network);
 }
 
 runTest(network).then(() => { process.exit(0); });
